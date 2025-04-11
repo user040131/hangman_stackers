2faae513b3929e3b2f3d6214b3e1acf2e2d47f74
WhileTrue <seungjub270@gmail.com>
2025-04-11 12:05:19 +0900

    globlaExceptionHandler add

--- src/main/java/me/WhileTrue/hangman/dto/DifficultyDTO.java
+++ src/main/java/me/WhileTrue/hangman/dto/DifficultyDTO.java
@@ -7,8 +7,8 @@ import lombok.Data;
 @Data
 public class DifficultyDTO {
 
-    @Min(value = 0, message = "난이도는 0 이상이어야 합니다.")
-    @Max(value = 2, message = "난이도는 2 이하여야 합니다.")
+    @Min(value = 0, message = "난이도는 0 이상, 2이하이어야 합니다.")
+    @Max(value = 2, message = "난이도는 0 이상, 2이하이어야 합니다.")
     private int difficulty;
 
     public int getDifficulty() {
